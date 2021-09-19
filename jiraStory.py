24c4660868cc3a16b34a923c4130e6bac47b38cd
PrashantKumarGit <85401464+PrashantKumarGit@users.noreply.github.com>
2021-09-19 17:26:47 +0530

    Update jiraStory.py

--- jiraStory.py
+++ jiraStory.py
@@ -1,1 +1,3 @@
-print("Hello, This is Jira story #987654321. \n"+"It has below changes:-\n"+"a) error fixed.\n b)new features added.")
+import sys
+
+print("Hello, This is Jira story #987654321. \n"+"It has below changes:-\n"+"a) error fixed.\n b) new features added.\n c) sys.argv[0]")
