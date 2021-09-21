b64d865b1cfcd680d6888fabdc57074dc034a5c4
PrashantKumarGit <85401464+PrashantKumarGit@users.noreply.github.com>
2021-09-21 15:12:27 +0530

    Update jiraStory.py

--- jiraStory.py
+++ jiraStory.py
@@ -1,4 +1,4 @@
 import sys
 
-jira_desc = "Hello, This is Jira story: "+sys.argv[1]+". \n"+"It has below changes:-\n"+"a) error fixed.\n b) new features added."
+jira_desc = "This is Jira story: {}\n"+"It has below changes:-\n"+"a) error fixed.\n b) new features added.".format(sys.argv[1])
 print(jira_desc)
